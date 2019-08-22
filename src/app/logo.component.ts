ab8a7354bd94f0ed6366397c31b5f36105f07055
Israel Guzman <isragp4@gmail.com>
2019-08-22 17:00:11 -0400

    removes unnecessary selector

--- src/app/logo.component.ts
+++ src/app/logo.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'hdev-logo',
   template: `
     <div style="text-align: center">
       <img width="300" alt="Logo" src="../assets/{{imgUrl}}.png">
