9fa80e677a4fc17983de088be8dbff13997d98f0
Israel Guzman <isragp4@gmail.com>
2019-08-22 17:36:43 -0400

    Adds social media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="text-align:center">
+      <h1>Angular Elements de Zero a Héroe</h1>
+      
+      <img width="300" alt="NG Bolivia Logo" src="../assets/ng-bo.png">
+
+      <h2>Israel Guzman</h2>
+      <a target="_blank"
+         href="https://twitter.com/israelgp4"
+         color="primary"
+         mat-mini-fab aria-label="Example icon-button with a heart icon">
+        <mat-icon svgIcon="twitter"></mat-icon>
+      </a>
+      <h2>@israelgp4</h2>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'ws2019';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatIconRegistry } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,22 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'twitter',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/twitter.svg')
+    );
+  }
+}
