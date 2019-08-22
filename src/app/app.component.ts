f93d83372a233e5babc04c36714d79c4912db690
Israel Guzman <isragp4@gmail.com>
2019-08-22 16:33:34 -0400

    Adds everything needed for the exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,4 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Angular Elements de Zero a Héroe';
-  imgUrl = 'ng-bo';
-  showComponent = false;
-
-  toggle() {
-    this.showComponent = !this.showComponent;
-    this.setData();
-  }
-
-  private setData() {
-    if (!this.showComponent) {
-      this.title = 'Angular Elements de Zero a Héroe';
-      this.imgUrl = 'ng-bo';
-    } else {
-      this.title = 'RxJs Live';
-      this.imgUrl = 'rxjs-live-logo';
-    }
-  }
 }
--- src/app/logo.component.ts
+++ src/app/logo.component.ts
@@ -1,19 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'hdev-logo',
   template: `
     <div style="text-align: center">
-      <img width="300" alt="Logo" src="../assets/{{imgUrl}}.png">
-      <h1>{{title}}</h1>
+      <img width="300" alt="Logo" src="../assets/">
+      <h1></h1>
     </div>
   `
 })
 
 export class HdevLogoComponent {
-  @Input()
-  title: string;
 
-  @Input()
-  imgUrl: string;
 }
